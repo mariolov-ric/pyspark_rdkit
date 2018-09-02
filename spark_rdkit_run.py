27f45c4a23743ababb9c0209a5e185a028239c66
Mario Lovric <mlovric@know.know-center.at>
2018-09-02 19:35:39 +0200

    update documented version

--- spark_rdkit_run.py
+++ spark_rdkit_run.py
@@ -1,101 +1,141 @@
 '''
-Author: Mario Lovric, Know-Center, Austria
-'''
-from datetime import datetime
-startTime = datetime.now()
-#imports
-import numpy as np
-import os
-os.environ['SPARK_MAJOR_VERSION'] = "1"
-os.environ['PYSPARK_PYTHON'] = "/usr/bin/python"
+Author: Mario Lovric, Know-Center, Graz, Austria
 
+MIT License
+Copyright (c) 2018 Mario Lovric
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+
+
+"""This file contains the code described in the journal article in
+   Molecular Informatics: https://doi.org/10.1002/minf.201800082
+
+The main guard largely contains code to parse commandline arguments and
+setup the spark context. The experiment is run for a various amount of
+instance sizes in order to compare their run times.
+
+Run pyspark park_rdkit_run.py -h in order to get the number of
+required arguments.
+
+"""
+
+import time
+import os
+import json
+from datetime import datetime
+import argparse
+
+import numpy as np
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
-
-conf=SparkConf().setAppName('app').setMaster('yarn-client').setAll([('spark.executor.memory', '80g'), ('spark.executor.cores', '12'), ('spark.cores.max', '12'), ('spark.driver.memory','80g')])
-sc = SparkContext(conf=conf)
-sqlContext =SQLContext(sc)
 from rdkit import Chem
 from rdkit.Chem import Descriptors
 from rdkit.ML.Descriptors import MoleculeDescriptors
-import time
+
+from utils import (rdd2smile, clean, comp_desc, desc_dict)
 
 
-def rdd2smile(x):
-    return x[0].encode("ascii", "ignore")
-def clean(y):
-    if y is None:
-        return None
-    chx=Chem.MolFromSmiles(rdd2smile(y))
-    if chx is None:
-        return None
-    return Chem.MolToSmiles(chx, isomericSmiles=True)
-def compDesc(smiles, calculator):
-    try:
-        mol=Chem.MolFromSmiles(smiles,sanitize=False)
-        Chem.SanitizeMol(mol)
-        result = np.array(calculator.CalcDescriptors(mol))
-        if np.all(result==-666):
-            return np.array([np.nan]*len(descriptors))
-        if np.all(np.isfinite(result)) == False:
-            return np.array([np.nan]*len(descriptors))
-        return result
-    except:
-        return np.array([np.nan]*len(descriptors))
-def desc_dict(x):
-    d = {}
-    for i in range(len(x)):
-        d[descriptors[i]] = str(x[i])
-    return d
+def run_experiment(filepath):
+    """Run experiment for comparison on runtime for different amount of compounds as SMILES strings.
 
-#list of descirptors
-descriptors = list(np.array(Descriptors._descList)[:,0])
-#define descriptor calculator
-calculator = MoleculeDescriptors.MolecularDescriptorCalculator(descriptors)
-
-#read smiles into DF
-df=sqlContext.read.parquet('/user/mlovric/pubchem_2mil.parquet')
-
-if os.path.exists('ex_time.txt'):
-    os.remove('ex_time.txt')
-
-for i in [100000,200000,500000,1000000, 1500000, 2000000, 2500000]:
-    #create rdd, limit or not
-    smiles_rdd=df.select('can_smiles').limit(i).rdd.repartition(10)
-    # clean smiles
-    cleaned_rdd = smiles_rdd.map(clean)
-    # remove nans in df
-    cleaned_df = cleaned_rdd.map(lambda x: (x,)).toDF(['smiles']).dropna(thresh=1, subset=('smiles'))
-    # cleaned_df = cleaned_df.dropna(thresh=1,subset=('smiles'))
-    # create clean rdd dropped nan
-    smiles_clean_rdd = cleaned_df.rdd.repartition(10)
-    #queries
-    ######
-    startQuery=datetime.now()
-    ######
-    mol_rdd = smiles_clean_rdd.map(lambda x: Chem.MolFromSmiles(rdd2smile(x)))
-    sub_rdd = mol_rdd.map(lambda x: x.HasSubstructMatch(Chem.MolFromSmiles('CO')))
-    Count=sub_rdd.collect().count(True)
-    ######
-    endQuery=datetime.now()
-    ######
-
-    #calculate descriptors
-    desc_data_rdd=smiles_clean_rdd.map(lambda x: compDesc(rdd2smile(x),calculator))
-    #create dataframe with descriptors
-    descriptors_df = desc_data_rdd.map(lambda x: desc_dict(x)).toDF(descriptors)
-    #write file
-    descriptors_df.write.parquet('descriptor_data_v'+str(time.time())+'.parquet')
-    ######
-    savingTime=datetime.now()###
-    ######
-    F=open('ex_time.txt','a')
-    exDict={'test time':str(datetime.now()),'molecules':str(i),\
-            'totalTime':str(datetime.now() - startTime),\
-            'queryTime':str(endQuery - startQuery),\
-            'descTime':str(savingTime - endQuery), \
-            'queryResultTrue':str(Count)}
-    F.write(str(exDict) + '\n')
-    F.close()
+    The experiment is run for a various number block sizes.
 
 
+    Parameters:
+    ----------
+    filepath : str path to parquet file
+    """
+
+    # list of descriptors from the RDKit Descriptors modules
+    descriptors = list(np.array(Descriptors._descList)[:, 0])
+    # the calculator module from RDKit
+    calculator = MoleculeDescriptors.MolecularDescriptorCalculator(descriptors)
+    df = sql_context.read.parquet(filepath)
+    sqlContext.read.parquet
+
+    with open('logfile.json', 'w') as json_file:
+        for num_lines in [1e5, 2e5, 5e5, 1e6, 1.5e6, 2e6]:
+            smiles_rdd = df.select('can_smiles') \
+                .limit(num_lines).rdd.repartition(10)
+            # map the previously defined function to the SMILES RDD
+            cleaned_rdd = smiles_rdd.map(clean)
+            # convert rdd to data frame, remove None values, assign to cleaned_df
+            cleaned_df = cleaned_rdd.map(
+                lambda x: (x,)).toDF(['smiles']) \
+                .dropna(thresh=1, subset=('smiles'))
+            smiles_clean_rdd = cleaned_df.rdd.repartition(10)
+            start_query = datetime.now()
+            # create RDD of MOL by using the map module and
+            # MolFromSmiles from RDKit, run cleaning again
+            mol_rdd = smiles_clean_rdd.map(
+                lambda x: Chem.MolFromSmiles(rdd2smile(x)))
+            # assign results of the query for substructure 'CO' to sub_rdd
+            # documentation from RDKit:
+            # http://www.rdkit.org/Python_Docs/rdkit.Chem.rdchem.Mol-class.html#HasSubstructMatch
+            sub_rdd = mol_rdd.map(
+                lambda x: x.HasSubstructMatch(Chem.MolFromSmiles('CO')))
+            count = sub_rdd.collect().count(True)
+            end_query = datetime.now()
+            desc_data_rdd = smiles_clean_rdd.map(
+                lambda x: comp_desc(rdd2smile(x), calculator))
+            descriptors_df = desc_data_rdd.map(
+                lambda x: desc_dict(x)).toDF(descriptors)
+            descriptors_df.write.parquet('descriptor_data_v'
+                                         + str(time.time())
+                                         + '.parquet')
+            ex_dict = {
+                'test time': str(datetime.now()),
+                'molecules': str(num_lines),
+                'totalTime': str(datetime.now() - start_time),
+                'queryTime': str(end_query - start_query),
+                'descTime': str(datetime.now() - end_query),
+                'queryResultTrue': str(count)
+            }
+
+            json_file.dump(ex_dict)
+            json_file.dump('\n')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', help='path to parquet file',
+                        type=str)
+    parser.add_argument('interpreter', help='interpreter to be used to'
+                        + 'run the experiments.', type=str)
+    args = parser.parse_args()
+    path = args.filepath
+    interpreter = args.interpreter
+
+    os.environ['SPARK_MAJOR_VERSION'] = '1'
+    os.environ['PYSPARK_PYTHON'] = interpreter
+
+    conf = (SparkConf()
+            .setAppName('app')
+            .setMaster('yarn-client')
+            .setAll([('spark.executor.memory', '80g'),
+                     ('spark.executor.cores', '12'),
+                     ('spark.cores.max', '12'),
+                     ('spark.driver.memory', '80g')]))
+    sc = SparkContext(conf=conf)
+    sql_context = SQLContext(sc)
+
+    start_time = datetime.now()
+    run_experiment(path)
---
+++ utils.py
@@ -0,0 +1,114 @@
+'''
+Author: Mario Lovric, Know-Center, Graz, Austria
+
+MIT License
+Copyright (c) 2018 Mario Lovric
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+
+"""This file contains helper functions used in the experiment
+
+"""
+
+from rdkit import Chem
+
+
+def rdd2smile(x):
+    """converts SMILES as rdd's to SMILES as strings.
+
+    Parameters:
+    ----------
+     x SMILES in form of RDD
+
+    Returns:
+    ----------
+    SMILES in form string
+    """
+    return x[0].encode("ascii", "ignore")
+
+def clean(y):
+    """Uses the function rdd2smiles to convert RDD SMILES to SMILES
+    string, if there is a value, otherwise it converts the SMILES
+    string to a MOL file format assigned to the chx variable. The
+    molecule in MOL format is subsequently converted back to an
+    isomeric SMILES.
+
+    Parameters
+    ----------
+    y RDD SMILES
+
+    Returns
+    ----------
+    cleaned isomeric SMILES
+    """
+    if y is None:
+        return None
+    chx = Chem.MolFromSmiles(rdd2smile(y))
+    if chx is None:
+        return None
+    return Chem.MolToSmiles(chx, isomericSmiles=True)
+
+def comp_desc(smiles, calculator):
+    """ Calculates descriptors for a given compunds as SMILES strings
+    Parameters
+    ----------
+    smiles SMILES strings of a compound
+    calculator the calculator module
+
+    Returns
+    ----------
+    array of descriptor values for the compound
+    """
+    try:
+        # assign converted structure from SMILES to MOL format
+        mol = Chem.MolFromSmiles(smiles, sanitize=False) 
+        # sanitize structure, module import from rdkit
+        Chem.SanitizeMol(mol) 
+        # assign result of descriptor calculation
+        result = np.array(calculator.CalcDescriptors(mol)) 
+         # RDKit returns -666 in case it is not able to return a valid value, please visit
+         # https://github.com/rdkit/rdkit/search?q=-666&unscoped_q=-666
+        if np.all(result == -666):
+            # return an array of None values times the length of the descriptor list
+            return np.array([np.nan]*len(descriptors)) 
+        # if the array of descriptors is not finite,
+        if np.all(np.isfinite(result)) == False: 
+            # return an array of None values times the length of the descriptor list
+            return np.array([np.nan]*len(descriptors)) 
+        return result
+    except:
+        # if the functions throws an exception, return an array of None values
+        return np.array([np.nan]*len(descriptors))
+
+def desc_dict(x):
+    """ Returns a dictionary where keys are descriptors names and values are calculated descriptors
+    Parameters
+    ----------
+    x list of calculated descriptor values
+
+    Returns
+    ----------
+    dictionary containing descriptors names and values
+    """
+    d = {}
+    for i in range(len(x)):
+        d[descriptors[i]] = str(x[i])
+    return d
